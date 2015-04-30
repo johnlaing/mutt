cef61967670f9e8bf6e00fe5f722e8b6bf6e2de4
John Laing <john.laing@gmail.com>
2015-04-30 14:27:31 -0400

    add support for attachments

--- R/mutt.R
+++ R/mutt.R
@@ -1,9 +1,12 @@
-mutt <- function(to=c(), subject=NULL, body=NULL, body.file=NULL, body.html=FALSE, cc=NULL, bcc=NULL, from=NULL) {
+mutt <- function(to=c(), subject=NULL, body=NULL, body.file=NULL, body.html=FALSE, cc=NULL, bcc=NULL, from=NULL, attachments=NULL) {
     args <- character()
     if (body.html) args <- c(args, "-e", shQuote("my_hdr Content-Type: text/html"))
     if (!is.null(subject)) args <- c(args, "-s", shQuote(as.character(subject)))
     if (!is.null(cc) & length(cc) > 0) args <- c(args, rbind("-c", shQuote(as.character(cc))))
     if (!is.null(bcc) & length(bcc) > 0) args <- c(args, rbind("-b", shQuote(as.character(bcc))))
+    if (!is.null(attachments) & length(attachments) > 0) {
+        args <- c(args, rbind("-a", shQuote(as.character(attachments))))
+    }
     if (length(to) > 0) args <- c(args, "--", shQuote(to))
 
     if (!is.null(body)) {
